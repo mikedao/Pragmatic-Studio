f41846f3e5367eb8dba6c415f87718dd230f97c8
Michael Dao <michael.dao@gmail.com>
2014-10-08 17:01:15 -0400

    Lesson 4 - Variables and Objects - Complete

--- crowdfund.rb
+++ crowdfund.rb
@@ -3,10 +3,12 @@ name2 = 'LMN'
 name3 = 'XYZ'
 
 funding1 = 1000
+funding2 = 500
+funding3 = 2000
 
-puts "Project #{name1} has $#{funding1} in funding."
 
-puts "\nProjects: \n"
-puts "\t Project #{name1}"
-puts "\t Project #{name2}"
-puts "\t Project #{name3}"
+left = "Project".ljust(50)
+puts "#{left} Funding"
+puts "Project #{name1.ljust(45)} #{funding1}"
+puts "Project #{name2.ljust(45)} #{funding2}"
+puts "Project #{name3.ljust(45)} #{funding3}"
--- studio_game.rb
+++ studio_game.rb
@@ -1,30 +1,39 @@
-name = 'larry'
+name1 = "larry"
+name2 = "curly"
+name3 = "moe"
+name4 = "shemp"
 
-health = 60
+health1 = 60
+health2 = 125
+health3 = 100
+health4 = 90
 
-puts "#{name}'s health is #{health}"
+puts "#{name1.capitalize} has a health of #{health1}."
+puts "#{name2.upcase} has a health of #{health2}."
 
-# triple the health
-puts "#{name}'s health is #{health*3}"
+# Add two lines to the bottom of the program file: change Curly's health variable to point to Larry's health 
+# variable and print out Curly's information again.
+puts "\n"
+health2 = health1
+puts "#{name2.upcase} has a health of #{health2}."
 
-# divide by nine integer
-puts "#{name}'s health is #{health/9}"
+# Reassign Larry's health variable to be 30. Then print out the name and health of both players again.
+puts "\n"
+health1 = 30
+puts "#{name1.capitalize} has a health of #{health1}."
+puts "#{name2.upcase} has a health of #{health2}."
 
-# divide by nine decimal
-puts "#{name}'s health is #{health/9.0}"
 
-# printing out a list of players formatted
-puts "\nPlayers: \n"
-puts "\tlarry"
-puts "\tcurly"
-puts "\tmoe"
+# Create variables for a third player with the name "moe" and an initial health of 100. 
+puts "\n"
+display = "#{name3.capitalize} has a health of #{health3}."
+puts "#{display.center(50,"*")}"
 
-# printing out a list of players with variables and formatted
-name1 = 'larry'
-name2 = 'curly'
-name3 = 'moe'
+# Create a fourth player named "shemp" with an initial health of 90. Use variables to print out 
+# his information with the name capitalized and left-justified
+puts "\n"
+puts "#{name4.capitalize.ljust(20,".")} #{health4} health"
 
-puts "\nPlayers: \n"
-puts "\t#{name1}"
-puts "\t#{name2}"
-puts "\t#{name3}"
+puts "\n"
+current_time = Time.new
+puts "The game started on #{current_time.strftime("%A %D at %I:%M%p")}"
