9d919f139b290542984d0914b1c36cd94ec813b7
Michael Dao <michael.dao@gmail.com>
2014-10-08 16:10:31 -0400

    Lesson 3 - Numbers and Strings - Complete

---
+++ crowdfund.rb
@@ -0,0 +1,12 @@
+name1 = 'ABC'
+name2 = 'LMN'
+name3 = 'XYZ'
+
+funding1 = 1000
+
+puts "Project #{name1} has $#{funding1} in funding."
+
+puts "\nProjects: \n"
+puts "\t Project #{name1}"
+puts "\t Project #{name2}"
+puts "\t Project #{name3}"
---
+++ studio_game.rb
@@ -0,0 +1,30 @@
+name = 'larry'
+
+health = 60
+
+puts "#{name}'s health is #{health}"
+
+# triple the health
+puts "#{name}'s health is #{health*3}"
+
+# divide by nine integer
+puts "#{name}'s health is #{health/9}"
+
+# divide by nine decimal
+puts "#{name}'s health is #{health/9.0}"
+
+# printing out a list of players formatted
+puts "\nPlayers: \n"
+puts "\tlarry"
+puts "\tcurly"
+puts "\tmoe"
+
+# printing out a list of players with variables and formatted
+name1 = 'larry'
+name2 = 'curly'
+name3 = 'moe'
+
+puts "\nPlayers: \n"
+puts "\t#{name1}"
+puts "\t#{name2}"
+puts "\t#{name3}"
